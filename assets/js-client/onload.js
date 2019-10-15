0f5855218c9d765be1220384e9a5d0c21d4ccb9d
Leonardo Matos <leomp120894@gmail.com>
2019-10-15 13:24:45 -0300

    feat(onload-expression): handle paypal buttons

--- assets/js-client/onload.js
+++ assets/js-client/onload.js
@@ -1,3 +1,19 @@
 ;(function () {
-
+  // https://developer.paypal.com/docs/checkout/integrate/#3-render-the-smart-payment-buttons
+  /* global paypal, _paypalOrderObj */
+  window._paypalApprove = new Promise(function (resolve) {
+    paypal.Buttons({
+      createOrder: function (data, actions) {
+        return actions.order.create(_paypalOrderObj)
+      },
+      onApprove: function (data, actions) {
+        // capture the funds from the transaction
+        return actions.order.capture().then(function (details) {
+          // show a success message to your buyer
+          window.alert('Transaction completed by ' + details.payer.name.given_name)
+          resolve()
+        })
+      }
+    }).render('#paypal-button-container')
+  })
 }())
