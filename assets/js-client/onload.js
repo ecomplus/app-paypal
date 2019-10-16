15d329e23a37966a20fcede3f58d3f8ce5d68427
Leonardo Matos <leomp120894@gmail.com>
2019-10-16 12:17:52 -0300

    feat(onload): return some PayPal order data

--- assets/js-client/onload.js
+++ assets/js-client/onload.js
@@ -6,12 +6,31 @@
       createOrder: function (data, actions) {
         return actions.order.create(_paypalOrderObj)
       },
+
       onApprove: function (data, actions) {
         // capture the funds from the transaction
         return actions.order.capture().then(function (details) {
-          // show a success message to your buyer
-          window.alert('Transaction completed by ' + details.payer.name.given_name)
-          resolve()
+          // return some PayPal order data
+          // https://developer.paypal.com/docs/api/orders/v2/#orders_get
+          var i
+          var transaction = {
+            intermediator: {
+              transaction_id: details.id
+            }
+          }
+          if (Array.isArray(details.links)) {
+            for (i = 0; i < details.links.length; i++) {
+              var link = details.links[i]
+              if (link.rel === 'approve') {
+                transaction.payment_link = link.href
+                break
+              }
+            }
+          }
+          if (Array.isArray(details.purchase_units) && details.purchase_units.length) {
+            transaction.intermediator.transaction_reference = details.purchase_units[0].reference_id
+          }
+          resolve(transaction)
         })
       }
     }).render('#paypal-button-container')
