d7e8d50689924e7ada3c21f6d0211fe90ca629af
Leonardo Matos <leomp120894@gmail.com>
2019-10-17 11:29:42 -0300

    chore(list-payments): unset locale on paypal sdk (default)

--- routes/ecom/modules/list-payments.js
+++ routes/ecom/modules/list-payments.js
@@ -97,10 +97,8 @@ module.exports = appSdk => {
 
     // PayPal Checkout JS client
     // https://developer.paypal.com/docs/checkout/integrate/
-    const locales = params.lang.split('_')
     let paypalScript = 'https://www.paypal.com/sdk/js' +
-      `?client-id=${paypalClientId}&currency=${params.currency_id}` +
-      `&locale=${locales[0]}_${(locales[1] || locales[0]).toUpperCase()}`
+      `?client-id=${paypalClientId}&currency=${params.currency_id}`
     if (config.paypal_debug) {
       paypalScript += '&debug=true'
     }
