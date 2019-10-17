cae7f1df9195d3325ca8c986b5391b8531e46a3c
Leonardo Matos <leomp120894@gmail.com>
2019-10-17 19:43:00 -0300

    chore(assets): test paypal buttons with mocked payer info

--- assets/express-checkout/onload.js
+++ assets/express-checkout/onload.js
@@ -3,12 +3,56 @@
   var _newPaypalOrderObj = function () {
     if (_amount && _amount.total) {
       return {
+        intent: 'CAPTURE',
+        payer: {
+          name: {
+            given_name: 'PayPal',
+            surname: 'Customer'
+          },
+          address: {
+            address_line_1: '123 Fake Street',
+            address_line_2: 'Apt 2',
+            admin_area_2: 'San Jose',
+            admin_area_1: 'CA',
+            postal_code: '95121',
+            country_code: 'US'
+          },
+          email_address: 'customer@domain.com',
+          phone: {
+            phone_type: 'MOBILE',
+            phone_number: {
+              national_number: '14082508100'
+            }
+          }
+        },
+        purchase_units: [
+          {
+            amount: {
+              value: '15.00',
+              currency_code: 'USD'
+            },
+            shipping: {
+              address: {
+                address_line_1: '2211 N First Street',
+                address_line_2: 'Building 17',
+                admin_area_2: 'San Jose',
+                admin_area_1: 'CA',
+                postal_code: '95131',
+                country_code: 'US'
+              }
+            }
+          }
+        ]
+      }
+      /*
+      return {
         purchase_units: [{
           amount: {
             value: _amount.total.toString()
           }
         }]
       }
+      */
     }
     return _paypalOrderObj
   }
