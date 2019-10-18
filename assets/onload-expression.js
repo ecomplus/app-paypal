1c4d6f51a236fc4d59a6617cce7d58e2d57aec2e
Leonardo Matos <leomp120894@gmail.com>
2019-10-18 18:15:08 -0300

    feat(onload): parse checkout data to paypal order object

--- assets/onload-expression.js
+++ assets/onload-expression.js
@@ -1,57 +1,76 @@
 ;(function () {
   window._newPaypalOrderObj = function () {
-    if (window._amount && window._amount.total) {
-      /*
-      TODO: pass payer info on order create
-      return {
-        intent: 'CAPTURE',
-        payer: {
-          name: {
-            given_name: 'PayPal',
-            surname: 'Customer'
-          },
-          address: {
-            address_line_1: '123 Fake Street',
-            address_line_2: 'Apt 2',
-            admin_area_2: 'San Jose',
-            admin_area_1: 'CA',
-            postal_code: '95121',
-            country_code: 'US'
-          },
-          email_address: 'customer@domain.com',
-          phone: {
-            phone_type: 'MOBILE',
-            phone_number: {
-              national_number: '14082508100'
+    if (window._checkout) {
+      var amount = window._checkout.amount
+      var customer = window._checkout.customer
+      var items = window._checkout.items
+
+      if (amount && amount.total) {
+        // parse E-Com Plus checkout data to PayPal order object
+        // https://github.com/ecomclub/storefront-app/commit/f55a5ef1170d50beb68a314ef6c096d37fbe9643
+        var paypalOrder = {
+          intent: 'CAPTURE',
+          purchase_units: [{
+            amount: {
+              value: amount.total.toString(),
+              currency_code: 'BRL'
+            }
+          }]
+        }
+
+        if (Array.isArray(items) && items.length) {
+          if (items[0].currency_id) {
+            paypalOrder.purchase_units[0].currency_code = items[0].currency_id
+          }
+        }
+
+        if (customer) {
+          paypalOrder.payer = {
+            email_address: customer.main_email
+          }
+          if (customer.name && customer.name.given_name) {
+            paypalOrder.payer.name = {
+              given_name: customer.name.given_name
+            }
+            if (customer.name.middle_name && customer.name.family_name) {
+              paypalOrder.payer.name.surname = customer.name.middle_name + ' ' + customer.name.family_name
+            } else if (customer.name.middle_name || customer.name.family_name) {
+              paypalOrder.payer.name.surname = customer.name.middle_name || customer.name.family_name
             }
           }
-        },
-        purchase_units: [
-          {
-            amount: {
-              value: '15.00',
-              currency_code: 'BRL'
-            },
-            shipping: {
-              address: {
-                address_line_1: '2211 N First Street',
-                address_line_2: 'Building 17',
-                admin_area_2: 'San Jose',
-                admin_area_1: 'CA',
-                postal_code: '95131',
-                country_code: 'US'
+
+          if (Array.isArray(customer.phones) && customer.phones[0]) {
+            paypalOrder.payer.phone = {
+              phone_type: 'MOBILE',
+              phone_number: {
+                national_number: customer.phones[0].number.toString()
               }
             }
           }
-        ]
-      }
-      */
-      return {
-        purchase_units: [{
-          amount: {
-            value: window._amount.total.toString()
+
+          if (Array.isArray(customer.addresses)) {
+            var paypalAddress
+            for (var i = 0; i < customer.addresses.length; i++) {
+              var address = customer.addresses[i]
+              if (address.default) {
+                paypalAddress = {
+                  postal_code: address.zip,
+                  admin_area_1: address.province_code || address.province || '',
+                  admin_area_2: address.city,
+                  address_line_1: address.line_address ||
+                    (address.street ? address.street + ' ' + (address.number || 'S/N') : ''),
+                  address_line_2: address.complement || '',
+                  country_code: address.country_code || 'BR'
+                }
+                break
+              }
+            }
+            if (paypalAddress) {
+              paypalOrder.purchase_units[0].shipping = paypalOrder.payer.address = paypalAddress
+            }
           }
-        }]
+        }
+        return paypalOrder
       }
     }
     return window._paypalOrderObj
