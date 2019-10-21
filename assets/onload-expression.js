d06617a8d56487207e5d26d70bc5328339d97427
Leonardo Matos <leomp120894@gmail.com>
2019-10-21 16:02:38 -0300

    fix(onload): add open_payment_id with paypal order id

--- assets/onload-expression.js
+++ assets/onload-expression.js
@@ -17,7 +17,6 @@
             }
           }]
         }
-
         if (Array.isArray(items) && items.length) {
           if (items[0].currency_id) {
             paypalOrder.purchase_units[0].currency_code = items[0].currency_id
@@ -88,28 +87,9 @@
       onApprove: function (data, actions) {
         // capture the funds from the transaction
         return actions.order.capture().then(function (details) {
-          // return some PayPal order data
-          // https://developer.paypal.com/docs/api/orders/v2/#orders_get
-          var i
-          var transaction = {
-            intermediator: {
-              transaction_id: details.id,
-              transaction_code: details.id
-            }
-          }
-          if (Array.isArray(details.links)) {
-            for (i = 0; i < details.links.length; i++) {
-              var link = details.links[i]
-              if (link.rel === 'approve') {
-                transaction.payment_link = link.href
-                break
-              }
-            }
-          }
-          if (Array.isArray(details.purchase_units) && details.purchase_units.length) {
-            transaction.intermediator.transaction_reference = details.purchase_units[0].reference_id
-          }
-          resolve(transaction)
+          resolve({
+            open_payment_id: details.id
+          })
         })
       }
     }).render('#paypal-button-container')
