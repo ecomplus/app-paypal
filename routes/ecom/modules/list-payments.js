cfe999a59b714945dc7c30bff1cc27547aa06322
Leonardo Matos <leomp120894@gmail.com>
2019-11-10 04:05:15 -0300

    feat(paypal-plus): finally prepare list payments to setup paypal plus

--- routes/ecom/modules/list-payments.js
+++ routes/ecom/modules/list-payments.js
@@ -2,6 +2,10 @@
 
 // generate default payment gateway object
 const newPaymentGateway = require(process.cwd() + '/lib/new-payment-gateway')
+// parse list payments body from Mods API to PayPal model
+const parsePaymentBody = require(process.cwd() + '/lib/parse-payment-body')
+// create PayPal payment request
+const createPaypalPayment = require(process.cwd() + '/lib/paypal-api/create-payment')
 
 module.exports = appSdk => {
   return (req, res) => {
@@ -18,6 +22,7 @@ module.exports = appSdk => {
         message: 'PayPal Client ID is unset on app hidden data (merchant must configure the app)'
       })
     }
+    const paypalEnv = config.paypal_sandbox ? 'sandbox' : 'live'
 
     // params object follows list payments request schema:
     // https://apx-mods.e-com.plus/api/v1/list_payments/schema.json?store_id=100
@@ -32,112 +37,150 @@ module.exports = appSdk => {
 
     // start mounting response body
     // https://apx-mods.e-com.plus/api/v1/list_payments/response_schema.json?store_id=100
-    const paymentGateway = newPaymentGateway(params.lang)
-    // merge configured options to payment gateway object
-    ;['label', 'text', 'icon'].forEach(prop => {
-      if (config[prop]) {
-        paymentGateway[prop] = config[prop]
-      }
-    })
-
-    // check available discount by payment method
-    const paymentMethod = paymentGateway.payment_method
-    if (
-      config.discount &&
-      (!config.discount_payment_method || config.discount_payment_method === paymentMethod.code)
-    ) {
-      paymentGateway.discount = config.discount
-    }
-
-    // setup response object
     const response = {
-      payment_gateways: [paymentGateway]
+      payment_gateways: []
     }
 
-    const { discount } = paymentGateway
-    if (discount) {
-      if (discount.apply_at !== 'freight') {
-        // default discount option
-        const { value } = discount
-        const label = config.discount_option_label || paymentGateway.label
-        response.discount_option = { label, value }
-        // specify the discount type and min amount is optional
-        ;['type', 'min_amount'].forEach(prop => {
-          if (discount[prop]) {
-            response.discount_option[prop] = discount[prop]
-          }
-        })
-      }
+    const addPaymentGateway = (paypalPlus, paypalPayment) => {
+      // add payment gateway object to response
+      const paymentGateway = newPaymentGateway(params.lang, paypalPlus)
+      response.payment_gateways.push(paymentGateway)
 
-      if (discount.min_amount) {
-        // check amount value to apply discount
-        if (params.amount && params.amount.total < discount.min_amount) {
-          delete paymentGateway.discount
-        } else {
-          delete discount.min_amount
+      // merge configured options to payment gateway object
+      ;['label', 'text', 'icon'].forEach(prop => {
+        if (config[prop]) {
+          paymentGateway[prop] = config[prop]
         }
+      })
+
+      // check available discount by payment method
+      const paymentMethod = paymentGateway.payment_method
+      if (
+        config.discount &&
+        (!config.discount_payment_method || config.discount_payment_method === paymentMethod.code)
+      ) {
+        paymentGateway.discount = config.discount
       }
-    }
 
-    const installmentsOption = config.installments_option
-    if (installmentsOption && installmentsOption.max_number) {
-      // default configured installments option
-      response.installments_option = installmentsOption
+      const { discount } = paymentGateway
+      if (discount) {
+        if (discount.apply_at !== 'freight') {
+          // default discount option
+          const { value } = discount
+          const label = config.discount_option_label || paymentGateway.label
+          response.discount_option = { label, value }
+          // specify the discount type and min amount is optional
+          ;['type', 'min_amount'].forEach(prop => {
+            if (discount[prop]) {
+              response.discount_option[prop] = discount[prop]
+            }
+          })
+        }
 
-      if (totalValue) {
-        // map to payment gateway installments
-        paymentGateway.installment_options = []
-        const minInstallment = installmentsOption.min_installment || 5
-        for (let number = 2; number <= installmentsOption.max_number.length; number++) {
-          // check installment value and configured minimum
-          const value = totalValue / number
-          if (value >= minInstallment) {
-            paymentGateway.installment_options.push({
-              number,
-              value,
-              // PayPal only supports interest-free installments
-              tax: false
-            })
+        if (discount.min_amount) {
+          // check amount value to apply discount
+          if (params.amount && params.amount.total < discount.min_amount) {
+            delete paymentGateway.discount
           } else {
-            break
+            delete discount.min_amount
           }
         }
       }
-    }
 
-    // PayPal Checkout JS client
-    const jsClient = paymentGateway.js_client
-    // https://developer.paypal.com/docs/checkout/integrate/
-    const locales = params.lang.split('_')
-    let paypalScript = 'https://www.paypal.com/sdk/js' +
-      `?client-id=${paypalClientId}` +
-      `&currency=${params.currency_id}` +
-      `&locale=${locales[0]}_${(locales[1] || locales[0]).toUpperCase()}`
-    if (!config.enable_standard_card_fiels) {
-      paypalScript += '&disable-funding=card'
-    }
-    if (config.paypal_debug) {
-      paypalScript += '&debug=true'
-    }
-    jsClient.script_uri = paypalScript
+      const installmentsOption = config.installments_option
+      if (installmentsOption && installmentsOption.max_number) {
+        // default configured installments option
+        response.installments_option = installmentsOption
 
-    // add order amount on JS expression
-    const paypalOrder = {}
-    if (totalValue) {
-      paypalOrder.purchase_units = [{
-        amount: {
-          value: totalValue.toString()
+        if (totalValue) {
+          // map to payment gateway installments
+          paymentGateway.installment_options = []
+          const minInstallment = installmentsOption.min_installment || 5
+          for (let number = 2; number <= installmentsOption.max_number.length; number++) {
+            // check installment value and configured minimum
+            const value = totalValue / number
+            if (value >= minInstallment) {
+              paymentGateway.installment_options.push({
+                number,
+                value,
+                // PayPal only supports interest-free installments
+                tax: false
+              })
+            } else {
+              break
+            }
+          }
         }
-      }]
-    }
-    let onloadExpression = `window._paypalOrderObj=${JSON.stringify(paypalOrder)};`
-    if (config.enable_standard_card_fiels) {
-      // https://developer.paypal.com/docs/checkout/integration-features/standard-card-fields/
-      // standard card fields is disabled by default due to bugs (BR only ?)
-      onloadExpression += 'window._paypalStCardFields=true;'
-    }
-    jsClient.onload_expression = onloadExpression + jsClient.onload_expression
+      }
 
-    res.send(response)
+      // PayPal Checkout JS client
+      const jsClient = paymentGateway.js_client
+      const locales = params.lang.split('_')
+      let paypalScript
+
+      if (!paypalPlus) {
+        // https://developer.paypal.com/docs/checkout/integrate/
+        paypalScript = 'https://www.paypal.com/sdk/js' +
+          `?client-id=${paypalClientId}` +
+          `&currency=${params.currency_id}` +
+          `&locale=${locales[0]}_${(locales[1] || locales[0]).toUpperCase()}`
+        if (!config.enable_standard_card_fiels) {
+          paypalScript += '&disable-funding=card'
+        }
+        if (config.paypal_debug) {
+          paypalScript += '&debug=true'
+        }
+      } else {
+        // https://developer.paypal.com
+        // /docs/integration/paypal-plus/mexico-brazil/integrate-a-payment-selection-page/
+        paypalScript = 'https://www.paypalobjects.com/webstatic/ppplusdcc/ppplusdcc.min.js'
+      }
+      jsClient.script_uri = paypalScript
+
+      // setup global variables on client for onload expression
+      let onloadExpression = `window._paypalEnv="${paypalEnv}";`
+      if (paypalPayment && Array.isArray(paypalPayment.links)) {
+        // approval URL for PayPal Plus
+        // https://developer.paypal.com/docs/integration/paypal-plus/mexico-brazil/create-a-payment-request/
+        const linkObj = paypalPayment.links.find(({ rel }) => rel === 'approval_url')
+        if (linkObj) {
+          onloadExpression += `window._paypalApprovalUrl="${linkObj.href}";`
+        }
+      }
+      if (config.enable_standard_card_fiels) {
+        // https://developer.paypal.com/docs/checkout/integration-features/standard-card-fields/
+        // standard card fields is disabled by default due to bugs (BR only ?)
+        onloadExpression += 'window._paypalStCardFields=true;'
+      }
+
+      // add order amount on JS expression
+      const paypalOrder = {}
+      if (totalValue) {
+        paypalOrder.purchase_units = [{
+          amount: {
+            value: totalValue.toString()
+          }
+        }]
+      }
+      jsClient.onload_expression = onloadExpression +
+        `window._paypalOrderObj=${JSON.stringify(paypalOrder)};` +
+        jsClient.onload_expression
+    }
+
+    new Promise(resolve => {
+      if (config.enable_paypal_plus && params.lang === 'pt_br') {
+        // also add payment gateway for PayPal Plus
+        createPaypalPayment(parsePaymentBody(params))
+          .then(paypalPayment => addPaymentGateway(true, paypalPayment))
+          .finally(resolve)
+      } else {
+        resolve()
+      }
+    }).then(() => {
+      // common PayPal SPB payment
+      addPaymentGateway()
+      // finally send success response
+      res.send(response)
+    })
   }
 }
